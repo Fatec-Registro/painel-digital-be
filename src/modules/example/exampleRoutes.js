8fd4d808f3527bcec3c2000ff669915364f0ef25
unknown <viniciussouzaramospv@gmail.com>
2025-06-03 19:01:36 -0300

    feat: adicionando autenticação no modulo de exemplo

--- src/modules/example/exampleRoutes.js
+++ src/modules/example/exampleRoutes.js
@@ -1,21 +1,22 @@
 import express from "express";
 import exampleController from "./exampleController.js";
+import Auth from "../../middleware/Auth.js";
 
 const exampleRoutes = express.Router();
 
 //Endpoint: Listar todos os Exemplos
-exampleRoutes.get("/examples", exampleController.getAllExample);
+exampleRoutes.get("/examples",  Auth.Authorization, exampleController.getAllExample);
 
 //Endpoint: Cadastrar um novo Exemplo
-exampleRoutes.post("/example", exampleController.createExample);
+exampleRoutes.post("/example", Auth.Authorization, exampleController.createExample);
 
 //Endpoint: Deletar um Exemplo
-exampleRoutes.delete("/example/:id", exampleController.deleteExample);
+exampleRoutes.delete("/example/:id", Auth.Authorization, exampleController.deleteExample);
 
 //Endpoint: Atualizar um Exemplo
-exampleRoutes.patch("/example/:id", exampleController.updateExample);
+exampleRoutes.patch("/example/:id", Auth.Authorization, exampleController.updateExample);
 
 //Endpoint: Listar um exemplo especifico
-exampleRoutes.get("/example/:id", exampleController.getOneExample);
+exampleRoutes.get("/example/:id", Auth.Authorization, exampleController.getOneExample);
 
 export default exampleRoutes;
